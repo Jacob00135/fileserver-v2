c1202f61a0fed5df6a72a7742146042a2ae455f0
Jacob <2428207444@qq.com>
2023-07-07 10:39:05 +0800

    加入批处理运行文件

---
+++ run_demo.bat
@@ -0,0 +1,7 @@
+set FLASK_APP=file_server.py
+set FLASK_ENV=production
+set FLASK_DEBUG=0
+set FLASK_CONFIG=production
+set FLASK_ADMIN_PASSWORD=123456
+set FLASK_PAGE_MAX=20
+venv\Scripts\flask run -h 0.0.0.0 -p 5000
